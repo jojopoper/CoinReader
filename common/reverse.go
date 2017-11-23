cd81f3d7a23abb28392ff081fac50da2e77b57ae
jojopoper <jojopoper@163.com>
2017-11-23 18:30:56 +0800

    add slice reverse function

---
+++ common/reverse.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"reflect"
+	"sync"
+)
+
+// ReverseSlice : reverse a slice
+type ReverseSlice struct {
+	sync.Mutex
+}
+
+// R : reverse
+func (ths *ReverseSlice) R(s interface{}) {
+	ths.Lock()
+	defer ths.Unlock()
+	size := reflect.ValueOf(s).Len()
+	swap := reflect.Swapper(s)
+	for i, j := 0, size-1; i < j; i, j = i+1, j-1 {
+		swap(i, j)
+	}
+}
