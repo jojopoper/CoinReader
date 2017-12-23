19626b41a2b52c4d30f36e5bb016e7cba9dbab68
jojopoper <jojopoper@163.com>
2017-12-23 14:48:04 +0800

    remove

--- aex/historyEx.go
+++
@@ -1,8 +0,0 @@
-package aex
-
-// rdHistory : readout histroy datas from aex.com, datas saved in History datas
-func (ths *ReaderEx) rdHistory() bool {
-	ths.historyClt.SetClient(ths.Next().Get())
-
-	return ths.Reader.rdHistory()
-}
--- aex/orderEx.go
+++
@@ -1,7 +0,0 @@
-package aex
-
-func (ths *ReaderEx) rdOrders() bool {
-	ths.orderClt.SetClient(ths.Next().Get())
-
-	return ths.Reader.rdOrders()
-}
