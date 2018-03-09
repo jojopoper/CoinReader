11749a0c10c882122e0a5a0b42f160312d2a0784
jojopoper <jojopoper@163.com>
2018-03-09 14:59:10 +0800

    修改注释错误

--- binance/rd/historyEx.go
+++ binance/rd/historyEx.go
@@ -1,6 +1,6 @@
 package rd
 
-// rdHistory : readout histroy datas from aex.com, datas saved in History datas
+// rdHistory : readout histroy datas from binance.com, datas saved in History datas
 func (ths *ReaderEx) rdHistory() bool {
 	ths.historyClt.SetClient(ths.cltCle.Next().Get())
 
